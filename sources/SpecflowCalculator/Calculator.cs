5600a34d4bafcd6384ca079269f2799411b7f6ea
Pierre Large <plarge@positivethinking.tech>
2021-01-18 11:34:00 +0100

    Première implémentation avec Specflow

---
+++ sources/SpecflowCalculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace SpecflowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return this.FirstNumber + this.SecondNumber;
+        }
+    }
+}
